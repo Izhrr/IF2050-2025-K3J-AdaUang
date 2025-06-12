b1cd4cc2019a55fb35ec948e3fafc9c8b9967f0c
Izhrr <19623264@std.stei.itb.ac.id>
2025-06-12 14:28:32 +0700

    restructure: refactor

--- src/controllers/BaseController.java
+++
@@ -1,6 +0,0 @@
-package controllers;
-
-// Placeholder untuk logika controller umum
-public abstract class BaseController {
-    // Bisa dikembangkan untuk fitur global controller (misal: error handler, dsb)
-}
